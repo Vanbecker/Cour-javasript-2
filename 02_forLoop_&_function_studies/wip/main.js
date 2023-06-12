fc3d351425cb8c2df893f2cd80851cb281337132
Rémi Le Gall <126484877+Vanbecker@users.noreply.github.com>
2023-06-12 14:26:24 +0200

    aprem

--- 02_forLoop_&_function_studies/wip/main.js
+++ 02_forLoop_&_function_studies/wip/main.js
@@ -8,27 +8,88 @@
 //     console.log(argument)
 // }
 
-// EXEMPLE 1 let valeurRetourn = sum2args(8, 7);
 
 
+// let value1 = sum2args(12, 7);
+// let value2= sum2args2(value1, 2);
+// logMessage(value2);
 
-let value1 = sum2args(12, 7);
-let value2= sum2args2(value1, 2);
-logMessage(value2);
+
+// function sum2args(arg1, arg2) {
+
+//     return arg1 + arg2 
+// }
+
+// function sum2args2(a1, a2) {
+
+//     return a1 - a2
+// }
+
+// function logMessage(arg) {
+//     console.log(`le resultats des 2 fonctions précédenters est ${arg} !`)
+// }
 
 
-function sum2args(arg1, arg2) {
+// const myNumber =[4, 6, 13, 3];
+// let value1 = sum2args(12, 7);
+// let value2= sum2args2(value1, 2);
+// logMessage(value2);
 
-    return arg1 + arg2 
+
+// function sumElements(myArray) {
+//    let myResult = arg1 + arg2
+//    return myResult
+// }
+
+// function logMessage(a1, a2) {
+//     return a1 - a2
+// }
+
+
+// function logMessage(arg) {
+//     console.log(`le resultats des 2 fonctions précédenters est ${arg} !`)
+// }
+
+
+// const myNumbers =[4, 6, 13, 9, 12, 11];
+// const myNumbers2 =[41, 61, 13, 19, 12, 11];
+
+// const res = sumElements(myNumbers)
+// const res2 =sumElements(myNumbers2)
+ 
+// console.log(res)
+// console.log(res2)
+
+// function sumElements(arr){
+//     let result = 0;
+
+//     for (let i = 0; i < arr.length; i ++) {
+//         if(arr[i] % 2 === 0) 
+        
+//         result += arr[i];
+//     }
+
+//     return result;
+// }
+
+
+function giveMyFullName() {
+    let firstname = `Paul`
+    let lastname= `Doszan`
+    
+
+    let FullName = firstname + " " +lastname
+    return FullName;
 }
+let myName = giveMyFullName();
+console.log(myName)
 
-function sum2args2(a1, a2) {
+// for (let i = 2; i <= 10; i += 3) {
+//     if (i === 6) {
+//         break;
+//     }
+//     console.log(`Mon indice de boucle : ${i}`)
+// }
 
-    return a1 - a2
-}
-
-function logMessage(arg) {
-    console.log(`le resultats des 2 fonctions précédenters est ${arg} !`)
-}
-
+// console.log(`la boucle est terminée`)
 
