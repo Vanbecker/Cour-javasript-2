0d7972d05c7a1b9b2a1ff1bda90153bb0270c7da
Rémi Le Gall <126484877+Vanbecker@users.noreply.github.com>
2023-06-12 17:57:11 +0200

    index perdu

--- 02_forLoop_&_function_studies/wip/main.js
+++ 02_forLoop_&_function_studies/wip/main.js
@@ -132,44 +132,220 @@
 
 // 3. Ecrire une fonction qui retourne un tableau de nombres, tous superieur a la moyenne des elements du tableau en argument 
 
-function getNumbersSuperiorToAvg(arr) {
-    //on calcule la moyenne
-    let total = 0;
+// function getNumbersSuperiorToAvg(arr) {
+//     //on calcule la moyenne
+//     let total = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        total += arr[i]
+//     for (let i = 0; i < arr.length; i++) {
+//         total += arr[i]
+//     }
+
+
+//     let avg = total / arr.length;
+
+//     //deuxieme etape, on tri le tableau en fonction de la moyenne 
+//     let newArr = []
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > avg) {
+//             newArr.push(arr[i])
+//         }
+//     }
+
+//     return newArr;
+
+// }
+
+
+// //pour rajouter un element au tableau
+
+// word.push(`Bom dia`)
+// word.pop()
+// word.pop()
+// word.pop()
+
+// console.log(myVar);
+
+// // retire le dernier élément du tableau
+// const myVar = words.pop()
+
+// //retire le premier élément au tableau
+// words.unshift()
+
+// console.log(myVar);
+
+
+/////EXO PIZZA
+
+const pizzas = [
+    {
+        name: 'Margherita',
+        price: 11.50,
+        ingredients: ['mozzarella', 'tomate', 'basilic ', "huile d'olive"],
+        baseTomate: true
+    },
+    {
+        name: 'Regina',
+        price: 12,
+        ingredients: ['mozzarella', 'tomate', 'origan', 'jambon', 'champignons'],
+        baseTomate: true
+    },
+    {
+        name: '4 saisons',
+        price: 15,
+        ingredients: ['artichaut', 'courgette', 'poivron', 'mozzarella', 'oignon rouge'],
+        baseTomate: true
+    },
+    {
+        name: 'Napolitaine',
+        price: 14,
+        ingredients: ['tomate', 'mozzarella', 'anchois', 'olives noires', 'origan'],
+        baseTomate: true
+    },
+    {
+        name: '4 fromages',
+        price: 16,
+        ingredients: ['tomate', 'mozzarella', 'emmental', 'comté', 'roquefort'],
+        baseTomate: true
+    },
+    {
+        name: 'Montagnarde',
+        price: 19,
+        ingredients: ['mozzarella', 'reblochon', 'gruyère', 'oignon', 'champignon'],
+        baseTomate: false
+    },
+    {
+        name: 'Chèvre-miel',
+        price: 18,
+        ingredients: ['mozzarella', 'chèvre', 'miel'],
+        baseTomate: false
+    },
+    {
+        name: 'Hawaïenne',
+        price: 17,
+        ingredients: ['mozzarella', 'tomate', 'jambon', 'ananas'],
+        baseTomate: true
     }
+]
 
 
-    let avg = total / arr.length;
+// POUR LOG ELEMENT 1 DE PIZZA 1
+// console.log(pizza[1].ingredients[1])
 
-    //deuxieme etape, on tri le tableau en fonction de la moyenne 
-    let newArr = []
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > avg) {
-            newArr.push(arr[i])
-        }
+
+// # 1. écrire une fonction qui prend en parametre un tableau, cette fonction retourne tous les .name des elements du tableau dans un tableau names[]
+
+// const getNames = (arr) => {
+//     const names = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         names.push(arr[i].name);
+//     }
+
+//     return names;
+// }
+
+// const names = getNames(pizzas);
+// console.log(names);
+
+
+// AUTRE CORRECTION
+
+function getAllNames(tab) {
+    let pizzaNames = [];
+
+    for (let i = 0; i < tab.length; i++) {
+        pizzaNames.push(tab[i].name)
     }
-
-    return newArr;
-
+    return pizzaNames;
 }
 
 
-//pour rajouter un element au tableau
+// # 2. Ecrire une fonction qui prend en parametre le tableau pizzas et qui retourne le prix moyen des pizzas
 
-word.push(`Bom dia`)
-word.pop()
-word.pop()
-word.pop()
+// const getPizzaAveragePrice = (arr) => {
+//     let totalPrice = 0;
 
-console.log(myVar);
+//     for (let i = 0; i < arr.length; i++) {
+//         totalPrice += arr[i].price;
+//     }
 
-// retire le dernier élément du tableau
-const myVar = words.pop()
+//     const averagePrice = totalPrice / arr.length;
+//     return averagePrice;
+// }
 
-//retire le premier élément au tableau
-words.unshift()
+// const averagePrice = getPizzaAveragePrice(pizzas);
+// console.log(averagePrice);
 
-console.log(myVar);
+// AUTRE CORRECTION
+console.log(getAveragePrice(pizzas));
+function getAveragePrice(arr) {
+    let totalPrice = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        totalPrice += arr[i].price;
+    }
+    return (totalPrice / arguments.length).toFixed(2)
+}
+
+
+// # 3. Ecrire une fonction qui prend en parametre un nom de pizza et qui retourne un tableau des ingredients de cette pizza
+const getPizzaIngredients = (pizzaName) => {
+    const pizza = pizzas.find(pizza => pizza.name.toLowerCase() === pizzaName.toLowerCase());
+
+    if (pizza) {
+        return pizza.ingredients;
+    } else {
+        return [];
+    }
+}
+
+const pizzaName = 'Margherita';
+const ingredients = getPizzaIngredients(pizzaName);
+console.log(ingredients);
+
+// AUTRE CORRECTION
+
+
+
+
+
+// # 4. Ecrire une fonction qui prend en parametre un tableau de pizzas et qui retourne un tableau de tous les ingredients, en evitant les elements dupliques
+
+const getAllIngredients = (pizzaArr) => {
+    const ingredientsSet = new Set();
+
+    for (let i = 0; i < pizzaArr.length; i++) {
+        const ingredients = pizzaArr[i].ingredients;
+        for (let j = 0; j < ingredients.length; j++) {
+            ingredientsSet.add(ingredients[j]);
+        }
+    }
+
+    const uniqueIngredients = Array.from(ingredientsSet);
+    return uniqueIngredients;
+}
+
+const allIngredients = getAllIngredients(pizzas);
+console.log(allIngredients);
+
+
+// # 5. Ecrire une fonction qui prend en parametre un ingredient et qui retourne un tableau des noms des pizzas qui ont cet ingredient
+
+const getPizzasByIngredient = (ingredient) => {
+    const pizzasWithIngredient = [];
+
+    for (let i = 0; i < pizzas.length; i++) {
+        const pizzaIngredients = pizzas[i].ingredients;
+        if (pizzaIngredients.includes(ingredient)) {
+            pizzasWithIngredient.push(pizzas[i].name);
+        }
+    }
+
+    return pizzasWithIngredient;
+}
+
+const ingredient = 'mozzarella';
+const pizzasWithIngredient = getPizzasByIngredient(ingredient);
+console.log(pizzasWithIngredient);
